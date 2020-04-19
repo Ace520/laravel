7ade60c0308ee1a1ad116471b3fc4b2491eca153
Ace <ace963ace@gmail.com>
2020-04-20 00:07:38 +0800

    1

---
+++ app/Http/Controllers/Goods/GoodsDetailsController.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Admin\Controllers\Goods;
+
+use App\Models\Goods\GoodsDetails;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class GoodsDetailsController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header(trans('admin.index'))
+            ->description(trans('admin.description'))
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header(trans('admin.detail'))
+            ->description(trans('admin.description'))
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed $id
+     * @param Content $content
+     * @return Content
+     */
+    public function edit($id, Content $content)
+    {
+        return $content
+            ->header(trans('admin.edit'))
+            ->description(trans('admin.description'))
+            ->body($this->form()->edit($id));
+    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function create(Content $content)
+    {
+        return $content
+            ->header(trans('admin.create'))
+            ->description(trans('admin.description'))
+            ->body($this->form());
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new GoodsDetails);
+
+        $grid->id('ID');
+        $grid->goods_id('goods_id');
+        $grid->content('content');
+        $grid->created_at(trans('admin.created_at'));
+        $grid->updated_at(trans('admin.updated_at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(GoodsDetails::findOrFail($id));
+
+        $show->id('ID');
+        $show->goods_id('goods_id');
+        $show->content('content');
+        $show->created_at(trans('admin.created_at'));
+        $show->updated_at(trans('admin.updated_at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new GoodsDetails);
+
+        $form->display('ID');
+        $form->text('goods_id', 'goods_id');
+        $form->text('content', 'content');
+        $form->display(trans('admin.created_at'));
+        $form->display(trans('admin.updated_at'));
+
+        return $form;
+    }
+}
